ba62c6b9e02c896819befd4fc3c79abae31b85ee
Alex Rozanski <alex@rozanski.com>
2012-11-15 23:31:07 +0000

    Remove files that shouldn't be tracked by git

--- configureantlr.sh
+++ configureantlr.sh
@@ -4,7 +4,7 @@ cd grammar/antlr/libantlr3c
 # Configure the Makefile used for the ANTLR C library
 # Only configure if we haven't already (Makefile and antlr3config.h are outputs of this process)
 if [ ! -f antlr3config.h ] || [ ! -f Makefile ]; then
-    ./configure
+    ./configure --enable-64bit
 fi
 
 # Make the ANTLR C library
