3643eabcf1213854cbe2127dd61aa46ae9621e7c
Thomas Rooney <thomas@rooney.co>
2012-11-22 14:45:40 +0000

    Modified main.cpp such that it produces output for visual studio and modified antlr includes to disable debugger

--- main.cpp
+++ main.cpp
@@ -1,14 +1,37 @@
 #include "MAliceLexer.h"
 #include "MAliceParser.h"
 
+#ifdef _WIN32
+typedef unsigned char     uint8_t;
+typedef unsigned short    uint16_t;
+typedef unsigned int      uint32_t;
+typedef unsigned long int uint64_t;
+#endif
+
+
 // Skeleton file based on http://stackoverflow.com/a/8542203
-int main()
+int main(int argc,int *argv[])
 {
+    pANTLR3_UINT8      fName;
     uint8_t *bufferData;
     uint32_t bufferSize;
     pANTLR3_UINT8 bufferName;
+    pANTLR3_INPUT_STREAM       input;
 
-    pANTLR3_INPUT_STREAM input = antlr3StringStreamNew(bufferData, ANTLR3_ENC_8BIT, bufferSize, bufferName);
+    if (argc < 2 || argv[1] == NULL)
+    {
+        fName   =(pANTLR3_UINT8)"C:\\Code\\MaliceFinal\\malice\\input"; // Note in VS2005 debug, working directory must be configured
+    }
+    else
+    {
+        fName   = (pANTLR3_UINT8)argv[1];
+    }
+
+     // Create the input stream using the supplied file name
+     // (Use antlr38BitFileStreamNew for UTF16 input).
+     //
+    input  = antlr3FileStreamNew(fName, ANTLR3_ENC_UTF8);
+
     if (!input)
         return 1;
 
