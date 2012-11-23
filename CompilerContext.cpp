98593ee73c1a0024ce097a314b9b2628ed312ff2
Alex Rozanski <alex@rozanski.com>
2012-11-23 15:58:26 +0000

    Implement enterScope() and exitScope() on CompilerContext

--- CompilerContext.cpp
+++ CompilerContext.cpp
@@ -10,11 +10,50 @@ namespace MAlice {
         if (m_symbolTables.empty())
             return;
         
-        SymbolTable& innermostSymbolTable = m_symbolTables.back();
-        if (innermostSymbolTable.get(identifier))
+        SymbolTable* & innermostSymbolTable = m_symbolTables.back();
+        if (innermostSymbolTable->get(identifier))
             cerr << "Symbol conflict";
         
-        innermostSymbolTable.insert(identifier, entity);
+        innermostSymbolTable->insert(identifier, entity);
+    }
+    
+    bool CompilerContext::isSymbolInScope(std::string identifier, Entity const **outEntity)
+    {
+        if (m_symbolTables.empty())
+            return false;
+        
+        for (list<SymbolTable>::reverse_iterator iterator = m_symbolTables.rbegin(); iterator != m_symbolTables.rend(); iterator++) {
+            Entity const *entity = NULL;
+            
+            try {
+                entity = iterator->get(identifier);
+            }
+            catch(out_of_range e) {
+                if (entity && outEntity) {
+                    *outEntity = entity;
+                    return true;
+                }
+            }
+        }
+        
+        return false;
+     }
+    
+    void CompilerContext::enterScope()
+    {
+        m_symbolTables.push_back(new SymbolTable());
+    }
+    
+    void CompilerContext::exitScope()
+    {
+        // TODO: generate error if we pop too far.
+        if (m_symbolTables.empty())
+            return;
+        
+        SymbolTable* & lastSymbolTable = m_symbolTables.back();
+        delete lastSymbolTable;
+        
+        m_symbolTables.pop_back();
     }
     
 }; // namespace MAlice
--- CompilerContext.h
+++ CompilerContext.h
@@ -11,10 +11,14 @@ namespace MAlice {
 
     class CompilerContext{
     private:
-        std::list<SymbolTable> m_symbolTables;
+        std::list<SymbolTable*> m_symbolTables;
         
     public:
         void addEntityInScope(std::string identifier, Entity entity);
+        bool isSymbolInScope(std::string identifier, Entity const **outEntity);
+    
+        void enterScope();
+        void exitScope();
     }; // class CompilerContext
     
 }; // namespace MAlice
