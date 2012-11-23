b77a4201acfa15e713b53f29772056b6ff275a8c
Alex Rozanski <alex@rozanski.com>
2012-11-23 15:20:37 +0000

    Implement the basic symbol table methods

--- SymbolTable.cpp
+++ SymbolTable.cpp
@@ -8,6 +8,25 @@
 
 #include "SymbolTable.h"
 
+using namespace std;
+
 namespace MAlice {
+    const Entity *SymbolTable::get(string identifier)
+    {
+        unordered_map<string, Entity>::const_iterator iterator = m_symbolMap.find(identifier);
+        if (iterator == m_symbolMap.end())
+            return NULL;
+        
+        return &iterator->second;
+    }
     
+    void SymbolTable::insert(string identifier, Entity entity)
+    {
+        m_symbolMap.insert({identifier, entity});
+    }
+    
+    unsigned int SymbolTable::numberOfSymbols()
+    {
+        return (unsigned int)m_symbolMap.size();
+    }
 }; // namespace MAlice
