dee1c1c39afd1e0abef3e5658d42dbe4d8b1788a
Thomas Rooney <thomas@rooney.co>
2012-11-24 23:36:32 +0000

    removed old mutex code

--- src/CompilerContext.cpp
+++ src/CompilerContext.cpp
@@ -6,31 +6,6 @@
 
 using namespace std;
 
-int MUTEX_INIT(MUTEX *mutex)
-{
-    return -1;
-}
-
-int MUTEX_LOCK(MUTEX *mutex)
-{
-    #if defined(LINUX)
-        return pthread_mutex_lock( mutex );
-    #elif defined(WINDOWS)
-        return (WaitForSingleObject(*mutex, INFINITE)==WAIT_FAILED?1:0);
-    #endif
-    return -1;
-}
-
-int MUTEX_UNLOCK(MUTEX *mutex)
-{
-    #if defined(LINUX)
-        return pthread_mutex_unlock( mutex );
-    #elif defined(WINDOWS)
-        return (ReleaseMutex(*mutex)==0);
-    #endif
-    return -1;
-}
-
 namespace MAlice {
     
     CompilerContext::CompilerContext()
