0eacd85ebb410974f8846435e88613bda09de492
Alex Rozanski <alex@rozanski.com>
2012-12-06 13:42:54 +0000

    Implement Validation::validateVariableDeclarationNode() and use it in SemanticChecks

--- src/Validation.cpp
+++ src/Validation.cpp
@@ -1,10 +1,56 @@
 
 #include "Validation.h"
 
+#include "ASTWalker.h"
+#include "CompilerContext.h"
+#include "ErrorReporter.h"
+#include "ErrorFactory.h"
+#include "Utilities.h"
+#include "SemanticChecks.h"
+#include "VariableEntity.h"
+
 namespace MAlice {
 
 
     // Separator comment (for git).
+    
+    bool Validation::validateVariableDeclarationNode(ASTNode node, ASTWalker *walker, CompilerContext *ctx)
+    {
+        ASTNode identifierNode = Utilities::getChildNodeAtIndex(node, 0);
+        ASTNode typeNode = Utilities::getChildNodeAtIndex(node, 1);
+        
+        if (identifierNode != NULL) {
+            std::string identifier(Utilities::getNodeText(identifierNode));
+            
+            if (ctx->isKeyword(identifier)) {
+                Error *error = ErrorFactory::createSemanticError("Cannot declare variable '" + identifier + "' because it is a keyword.");
+                error->setLineNumber(Utilities::getNodeLineNumber(identifierNode));
+                
+                error->setArrowRanges(Utilities::rangeToSingletonList(
+                                                                      Utilities::createRange(Utilities::getNodeLineNumber(identifierNode), Utilities::getNodeColumnIndex(identifierNode))
+                                                                      ));
+                
+                ctx->getErrorReporter()->reportError(error);
+                
+                return false;
+            }
+            
+            checkSymbolNotInCurrentScopeOrOutputError(identifier, identifierNode, ctx);
+            
+            MAliceType type = Utilities::getTypeFromTypeString(std::string(Utilities::getNodeText(typeNode)));
+            
+            ctx->addEntityInScope(identifier, new VariableEntity(identifier, Utilities::getNodeLineNumber(identifierNode), type));
+            
+            // See if a third node exists - if so this is a variable assignment node that we want to visit
+            
+            ASTNode thirdNode = Utilities::getChildNodeAtIndex(node, 2);
+            if (thirdNode != NULL)
+                checkExpression(thirdNode, walker, ctx, type);
+            
+        }
+        
+        return true;
+    }
 
 
 }
--- src/Validation.h
+++ src/Validation.h
@@ -3,12 +3,19 @@
 #define _MALICEVALIDATION
 
 #include <iostream>
+#include "Types.h"
 
 namespace MAlice {
+    
+    class ASTWalker;
+    class CompilerContext;
+    
     namespace Validation {
         
         
         // Separator comment (for git).
+        
+        bool validateVariableDeclarationNode(ASTNode node, ASTWalker *walker, CompilerContext *ctx);
         
         
     } // namespace Validation
