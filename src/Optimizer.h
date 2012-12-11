81a4c0ec3fbd23ae06943f3939069570de30fe3e
Thomas Rooney <thomas@rooney.co>
2012-12-11 20:03:24 +0000

    Cross platform dirname and removed bitcode generator for favour of the calling the subprocess method to platform specific optimization

--- src/Optimizer.h
+++ src/Optimizer.h
@@ -1,15 +1,17 @@
 #ifndef _OPTIMIZER
 #define _OPTIMIZER
 #include "LLVMHeader.h"
+#include "Types.h"
+#include "CompilerContext.h"
 
 namespace MAlice {
     
     class Optimizer  {
     private:
         llvm::Module *m_module;
-    public: 
+    public:                
         Optimizer (llvm::Module*);
-        void constantFoldingPass();
+        void constantFoldingPass(ASTNode node, CompilerContext* ctx);
         llvm::Module *getModule();
     };
     
