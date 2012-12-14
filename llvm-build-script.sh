ee8c6807851dc98c21b776b7185efc3475fa1aba
Thomas Rooney <tr111@matrix17.doc.ic.ac.uk>
2012-12-14 13:54:41 +0000

    Build script for llvm

--- llvm-build-script.sh
+++ llvm-build-script.sh
@@ -4,13 +4,16 @@ mkdir build
 
 cd build
 ../llvm-3.0.src/configure --enable-optimized --disable-assertions
-make
+make -j
 cd ..
 mkdir llvm
 mv build/Release/* llvm
 mkdir llvm/include
 mkdir llvm/include/llvm
+
 mv llvm-3.0.src/include/llvm/* llvm/include/llvm/
-mv build/include/* llvm/include/llvm/
+
+cp -rfl build/include/llvm/ llvm/include/
+
 rm -rf build
 rm -rf llvm-3.0.src
--- src/LLVMHeader.h
+++ src/LLVMHeader.h
@@ -13,13 +13,13 @@
 #include "llvm/Support/Dwarf.h"
 #include "llvm/BasicBlock.h"
 //#include "llvm/Constant.h"
-#if defined _WIN32 || defined __APPLE__
+//#if defined _WIN32 || defined __APPLE__
 #include "llvm/Support/IRBuilder.h"
 #include "llvm/Analysis/DIBuilder.h"
 #include "llvm/Analysis/DebugInfo.h"
-#else
-#include "llvm/DIBuilder.h"
-#include "llvm/IRBuilder.h"
-#include "llvm/DebugInfo.h"
-#endif
+// #else
+//#include "llvm/DIBuilder.h"
+//#include "llvm/IRBuilder.h"
+//#include "llvm/DebugInfo.h"
+//#endif
 #endif
